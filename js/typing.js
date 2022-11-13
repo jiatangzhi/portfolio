be6981510c1d9ccb81a7b7193e0af0b2b053f9dd
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2022-11-13 20:59:00 +0000

    added stuff

---
+++ js/typing.js
@@ -0,0 +1,45 @@
+const typedTextSpan = document.querySelector("span.typed-text");
+const cursorSpan = document.querySelector(".typed-text-cursor");
+
+const textArray = [
+    "is looking for a job",
+    "enjoys problem solving",
+    "wants to get into Data Science",
+    "loves the gym!",
+    "is interested in Artificial Intelligence",
+    "loves nature",
+    "likes web design"]
+
+const typingDelay = 100;
+const erasingDelay = 60;
+const newTextDelay = 2000;
+let charIndex = 0;
+let textArrayIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        // if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        // if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) { textArrayIndex = 0; }
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+type();
