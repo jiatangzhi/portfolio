8acec0887565e7c13b08495ecce41fcd014fde7b
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-09 00:34:23 +0200

    changed background

--- js/typing.js
+++ js/typing.js
@@ -2,13 +2,14 @@ const typedTextSpan = document.querySelector("span.typed-text");
 const cursorSpan = document.querySelector(".typed-text-cursor");
 
 const textArray = [
-    "is looking for a job",
-    "enjoys problem solving",
-    "wants to get into Data Science",
-    "loves the gym!",
-    "is interested in Artificial Intelligence",
-    "loves nature",
-    "likes web design"]
+  "is looking for a job",
+  "enjoys problem solving",
+  "wants to get into Data Science",
+  "loves fitness!",
+  "is interested in Artificial Intelligence",
+  "loves travelling",
+  "likes web design",
+];
 
 const typingDelay = 100;
 const erasingDelay = 60;
@@ -17,29 +18,32 @@ let charIndex = 0;
 let textArrayIndex = 0;
 
 function type() {
-    if (charIndex < textArray[textArrayIndex].length) {
-        // if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, typingDelay);
-    }
-    else {
-        setTimeout(erase, newTextDelay);
-    }
+  if (charIndex < textArray[textArrayIndex].length) {
+    // if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
 }
 
 function erase() {
-    if (charIndex > 0) {
-        // if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
-        charIndex--;
-        setTimeout(erase, erasingDelay);
+  if (charIndex > 0) {
+    // if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) {
+      textArrayIndex = 0;
     }
-    else {
-        textArrayIndex++;
-        if (textArrayIndex >= textArray.length) { textArrayIndex = 0; }
-        setTimeout(type, typingDelay + 1100);
-    }
+    setTimeout(type, typingDelay + 1100);
+  }
 }
 
 type();
