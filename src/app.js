ed7139cd9a901955840f6a688a040b6114603d4e
Jia Xin Tang Zhi <jxt201@exeter.ac.uk>
2023-05-26 19:43:56 +0200

    added typing effect

--- src/app.js
+++ src/app.js
@@ -41,3 +41,26 @@ window.onscroll = () => {
 
   /*================sticky navbar=======*/
 };
+
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+
+ScrollReveal().reveal(".home-img, .project-box, .contact form", {
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+const typed = new Typed(".mutiple-text", {
+  strings: ["", "Frontend Developer", "Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  loop: true,
+});
